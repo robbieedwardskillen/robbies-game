a45a02ebfd39078be7182a16fb8236a2617a79f1
Robbie Skillen <robbieedwardskillen@gmail.com>
2023-03-08 14:07:54 -0500

    got emitter 2 set
    that's all

--- Assets/findAndAttachToPlayer.cs
+++ Assets/findAndAttachToPlayer.cs
@@ -14,7 +14,14 @@ public class findAndAttachToPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3 (players[0].transform.position.x,
-        players[0].transform.position.y, players[0].transform.position.z);
+        if (this.gameObject.name == "Emitter1"){
+            this.transform.position = new Vector3 (players[0].transform.position.x,
+            players[0].transform.position.y, players[0].transform.position.z);
+        }
+        if (this.gameObject.name == "Emitter2"){
+            this.transform.position = players[0].transform.position + players[0].transform.forward * 2f;
+            this.transform.rotation = players[0].transform.rotation;
+        }
+        
     }
 }
