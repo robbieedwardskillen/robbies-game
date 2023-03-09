41f81e7f5deeaaa4f854126246485a9f042ce967
Robbie Skillen <robbieedwardskillen@gmail.com>
2023-03-09 11:27:18 -0500

    found out how to enable liquids through my scripts
    that is all

--- Assets/findAndAttachToPlayer.cs
+++ Assets/findAndAttachToPlayer.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using com.zibra.liquid.Manipulators;
+
 public class findAndAttachToPlayer : MonoBehaviour
 {
     GameObject[] players;
+    ZibraLiquidEmitter zle;
     // Start is called before the first frame update
+
     void Start()
     {
+
+        zle = gameObject.GetComponent<ZibraLiquidEmitter>();
         players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (zle != null){
+            zle.enabled = false;
+        }
+
         if (this.gameObject.name == "Emitter1"){
             this.transform.position = new Vector3 (players[0].transform.position.x,
             players[0].transform.position.y, players[0].transform.position.z);
