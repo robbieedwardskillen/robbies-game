e3c20422760e3fc31e746f4948b8a1def980af41
Robbie Skillen <robbieedwardskillen@gmail.com>
2023-04-01 14:03:25 -0400

    made it so you can move the card list left and right with left and right bumper
    that s all

---
+++ Assets/MoveLeftOrRight.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftOrRight : MonoBehaviour
+{
+    private PlayerControls controls;
+    private float jump;
+    private float action1;private float action2;private float action3;private float action4;private float action5;
+	private float aim; private float shoot; private float blockDodge;
+	private float dpadUp; private float dpadDown; private float dpadLeft; private float dpadRight;
+	private float menu; 
+    private Vector2 move;
+	private Vector2 rotate;
+    private float posX = 0;
+    private Vector3 rt;
+    // Start is called before the first frame update
+    void Start()
+    {	
+		controls = new PlayerControls();
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+		controls.Gameplay.Move.performed += ctx => move = Vector2.zero;
+		controls.Gameplay.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
+		controls.Gameplay.Rotate.canceled += ctx => rotate = Vector2.zero;
+		controls.Gameplay.Jump.performed += ctx => jump = ctx.ReadValue<float>();
+		controls.Gameplay.Jump.canceled += ctx => jump = 0;
+		controls.Gameplay.Action1.performed += ctx => action1 = ctx.ReadValue<float>();
+		controls.Gameplay.Action1.canceled += ctx => action1 = 0;
+		controls.Gameplay.Action2.performed += ctx => action2 = ctx.ReadValue<float>();
+		controls.Gameplay.Action2.canceled += ctx => action2 = 0;
+		controls.Gameplay.Action3.performed += ctx => action3 = ctx.ReadValue<float>();
+		controls.Gameplay.Action3.canceled += ctx => action3 = 0;
+		controls.Gameplay.Action4.performed += ctx => action4 = ctx.ReadValue<float>();
+		controls.Gameplay.Action4.canceled += ctx => action4 = 0;
+		controls.Gameplay.Action5.performed += ctx => action5 = ctx.ReadValue<float>();
+		controls.Gameplay.Action5.canceled += ctx => action5 = 0;
+		controls.Gameplay.DPadUp.performed += ctx => dpadUp = ctx.ReadValue<float>();
+		controls.Gameplay.DPadUp.canceled += ctx => dpadUp = 0;
+		controls.Gameplay.DPadDown.performed += ctx => dpadDown = ctx.ReadValue<float>();
+		controls.Gameplay.DPadDown.canceled += ctx => dpadDown = 0;
+		controls.Gameplay.DPadLeft.performed += ctx => dpadLeft = ctx.ReadValue<float>();
+		controls.Gameplay.DPadLeft.canceled += ctx => dpadLeft = 0;
+		controls.Gameplay.DPadRight.performed += ctx => dpadRight = ctx.ReadValue<float>();
+		controls.Gameplay.DPadRight.canceled += ctx => dpadRight = 0;
+		controls.Gameplay.Aim.performed += ctx => aim = ctx.ReadValue<float>();
+		controls.Gameplay.Aim.canceled += ctx => aim = 0;
+		controls.Gameplay.Shoot.performed += ctx => shoot = ctx.ReadValue<float>();
+		controls.Gameplay.Shoot.canceled += ctx => shoot = 0;
+		controls.Gameplay.BlockDodge.performed += ctx => blockDodge = ctx.ReadValue<float>();
+		controls.Gameplay.BlockDodge.canceled += ctx => blockDodge = 0;
+        rt = GetComponent<RectTransform>().anchoredPosition;
+        posX = rt.x;
+		if (controls != null){
+			controls.Gameplay.Enable();
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (action5 == 1){
+            posX += 1f;
+            GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, rt.y, rt.z);
+        } 
+        if (action4 == 1){
+            posX -= 1f;
+            GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, rt.y, rt.z);
+        }
+    }
+}
